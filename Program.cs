9bdd3898b77af26f9c45a0abf598e3940c9d1176
David Leong Kam Yeong <alexandros_ff9@hotmail.com>
2020-10-13 16:38:11 +0800

    Added adam

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GitTutorial2
     {
         static void Main(string[] args)
         {
-            string msg = "Hello World!";
+            string msg = "Hello Adam and World!";
             Console.WriteLine(msg);
         }
     }
